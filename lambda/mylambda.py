4f37e20087d5b510af61b0f58db75a219d74e004
Michael Heitland <heitlm@amazon.com>
2020-08-16 20:00:11 +0200

    decode and unzip received cw log events in lambda

--- lambda/mylambda.py
+++ lambda/mylambda.py
@@ -1,8 +1,17 @@
+import gzip
 import json
+import base64
 
 def mylambda(event, context):
     print('Hello World!')
-    print(event)
+    cw_data = event['awslogs']['data']
+    compressed_payload = base64.b64decode(cw_data)
+    uncompressed_payload = gzip.decompress(compressed_payload)
+    payload = json.loads(uncompressed_payload)
+    log_events = payload['logEvents']
+    for log_event in log_events:
+        print(f'LogEvent: {log_event}')
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
